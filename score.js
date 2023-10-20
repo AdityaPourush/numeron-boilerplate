9fd0562b1e88d0aef952e20f6b868c1cc1930d60
AdityaPourush <adityapourush11@outlook.com>
2023-10-20 15:06:36 +0530

    Game Completed

--- score.js
+++ score.js
@@ -1,1 +1,12 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let score_box = document.getElementById("score-board");
+
+let value = localStorage.getItem("score");
+
+score_box.textContent = value;
+
+var again = document.querySelector(".play");
+
+again.onclick = () => {
+    location.href = "./game.html", "_self";
+}
